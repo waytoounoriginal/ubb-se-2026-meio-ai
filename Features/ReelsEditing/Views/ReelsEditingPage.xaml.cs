f5f62375c268159c63a4ea82475b6bed2c905b5f
tapordeimaia <maia.tapordei@gmail.com>
2026-04-08 20:54:40 +0300

    Remove copyright 2.0

--- Features/ReelsEditing/Views/ReelsEditingPage.xaml.cs
+++ Features/ReelsEditing/Views/ReelsEditingPage.xaml.cs
@@ -1,7 +1,3 @@
-// <copyright file="ReelsEditingPage.xaml.cs" company="PlaceholderCompany">
-// Copyright (c) PlaceholderCompany. All rights reserved.
-// </copyright>
-
 namespace ubb_se_2026_meio_ai.Features.ReelsEditing.Views
 {
     using System;
